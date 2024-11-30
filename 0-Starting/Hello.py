a4cc168d3816d37a9e7291f1fdaead3cfe10f3a0
Lilian <nespoulous.lilian@gmail.com>
2024-11-30 16:21:24 +0100

    Initial commit + ex00

---
+++ 0-Starting/Hello.py
@@ -0,0 +1,19 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list.pop()
+ft_list.append("World!")
+
+ft_tuple = ("Hello", "France!")
+
+ft_set.remove("tutu!")
+ft_set.add("Angouleme!")
+
+ft_dict["Hello"] = "42Angouleme!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
